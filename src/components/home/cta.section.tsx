385cb4eb0fff02f48b1a59b22b5bf90d472bb45b
IraHavryliuk <ihavryluik@gmail.com>
2025-09-30 15:50:16 +0300

    Update CTA section

--- src/components/home/cta.section.tsx
+++ src/components/home/cta.section.tsx
@@ -5,9 +5,9 @@ const CtaSection = () => {
       style={{ backgroundImage: "url(/home/cta.png)" }}
     >
       <div className="flex gap-4 items-center justify-center flex-col">
-        <h2 className="text-5xl font-bold text-white mb-6 text-center max-w-[585px] mx-auto leading-tight">Ready to have a decorated lifestyle?</h2>
-        <button className="rounded-lg px-6 py-3 font-bold text-black-600 shadow hover:bg-gray-100 transition bg-green-300" 
-        style={{ width: "200px", height: "60px", fontSize: "17px" }}>
+        <h2 className="text-5xl font-bold text-white mb-6 text-center w-xl  mx-auto leading-tight">Ready to have a decorated lifestyle?</h2>
+        <button className="rounded-lg px-6 py-3 font-bold text-black-600 w-52 h-16 text-lg shadow hover:bg-gray-100 transition bg-green-300" 
+        >
           Start Shopping
         </button>
       </div>
